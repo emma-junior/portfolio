4c22edff9fcf521fa6ea9f843c902829b197abff
emma-junior <emnzboy31@gmail.com>
2022-12-07 17:47:46 +0100

    ui bug fix

--- components/navbtn/Navbtn.js
+++ components/navbtn/Navbtn.js
@@ -5,22 +5,22 @@ const Navbtn = () => {
   const { dropdown } = useGlobalContext();
   return (
     <button
-      class="button-three"
+      className="button-three"
       aria-controls="primary-navigation"
       aria-expanded={dropdown}
     >
       <svg
         stroke="var(--button-color)"
         fill="none"
-        class="hamburger"
+        className="hamburger"
         viewBox="-10 -10 120 120"
         width="50"
       >
         <path
-          class="line"
-          stroke-width="10"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          className="line"
+          strokeWidth="10"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           d="m 20 40 h 60 a 1 1 0 0 1 0 20 h -60 a 1 1 0 0 1 0 -40 h 30 v 70"
         ></path>
       </svg>
