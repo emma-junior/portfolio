fa61b28b8f45e61b6955f5623cba55060ef0ffef
emma-junior <emnzboy31@gmail.com>
2023-04-09 19:41:58 +0100

    fixed bug in navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,20 +1,12 @@
-import React, { useRef } from "react";
-import { useGlobalContext } from "../hooks/context";
+import React from "react";
 
-const Navbar = () => {
-  const { dropdown, setDropdown } = useGlobalContext();
-  const hamBurger = useRef();
-
+const Navbar = ({ showMenu, hamBurger }) => {
   const style = {
-    btn: "before:block before:mb-[8px] before:w-[32px] before:h-[2px] before:bg-white before:rounded-lg after:block after:w-[32px] after:h-[2px] after:mt-[8px] after:bg-white  after:rounded-lg before:transition before:ease-in-out before:duration-300 after:transition after:ease-in-out after:duration-300",
+    btn: "before:block before:mb-[8px] before:w-[32px] before:h-[3px] before:bg-white before:rounded-lg after:block after:w-[32px] after:h-[3px] after:mt-[8px] after:bg-white  after:rounded-lg before:transition before:ease-in-out before:duration-300 after:transition after:ease-in-out after:duration-300",
     // active:
     //   "before:translate-y-[7px] before:bg-white before:rotate-45 after:bg-white after:-translate-y-[5px] after:-rotate-45",
   };
 
-  const showMenu = () => {
-    setDropdown(!dropdown);
-    hamBurger.current.classList.toggle("hamburger_active");
-  };
   return (
     <div className="flex w-11/12 justify-between mx-auto h-16 items-center">
       <h2 className="text-[28px] font-medium">EmmaTechy</h2>
