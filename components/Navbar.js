d56d94891cae1c12ceddc5cbbe99437bc108b9bd
emma-junior <emnzboy31@gmail.com>
2023-04-10 13:47:14 +0100

    changed logo

--- components/Navbar.js
+++ components/Navbar.js
@@ -8,8 +8,8 @@ const Navbar = ({ showMenu, hamBurger }) => {
   };
 
   return (
-    <div className="flex w-11/12 justify-between mx-auto h-16 items-center">
-      <h2 className="text-[28px] font-medium">EmmaTechy</h2>
+    <div className="flex w-10/12 justify-between mx-auto h-16 items-center">
+      <h2 className="text-[28px] font-medium">E.E</h2>
       <p onClick={showMenu} className=" mt-2 cursor-pointer">
         <button
           className={`${style.btn} transition ease-in-out duration-300`}
