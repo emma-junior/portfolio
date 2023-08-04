c9919d3cd95fade1159d684976fda8655d179186
emma-junior <emnzboy31@gmail.com>
2023-08-04 22:03:36 +0100

    added logo

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,17 +1,17 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
+import logo from "../assets/my-logo-white.png";
 
 const Navbar = ({ showMenu, hamBurger }) => {
   const style = {
     btn: "before:block before:mb-[8px] before:w-[32px] before:h-[3px] before:bg-white before:rounded-lg after:block after:w-[32px] after:h-[3px] after:mt-[8px] after:bg-white  after:rounded-lg before:transition before:ease-in-out before:duration-300 after:transition after:ease-in-out after:duration-300",
-    // active:
-    //   "before:translate-y-[7px] before:bg-white before:rotate-45 after:bg-white after:-translate-y-[5px] after:-rotate-45",
   };
 
   return (
     <div className="flex w-10/12 justify-between mx-auto h-16 items-center">
       <Link href="/">
-        <h2 className="text-[28px] font-medium cursor-pointer">E.E</h2>
+        <Image src={logo} width={120} height={40} alt="" />
       </Link>
       <p onClick={showMenu} className="cursor-pointer">
         <button
